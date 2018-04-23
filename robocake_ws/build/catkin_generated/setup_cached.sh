2ac1f3432f5fd3f03e98dcaab096bb3ed3bf0189
mattroz <matveyrozanov@gmail.com>
2018-04-23 16:13:36 +0300

    Added already compiled catkin workspace for RoboCake using Gazebo and ROS

---
+++ robocake_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/matt/work/dev/ros/robocake_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/matt/work/dev/ros/robocake_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/matt/work/dev/ros/robocake_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/matt/work/dev/ros/robocake_ws/src:$ROS_PACKAGE_PATH"
---
+++ robocake_ws/build/mybot_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/matt/work/dev/ros/robocake_ws/src/mybot_control
+# Build directory: /home/matt/work/dev/ros/robocake_ws/build/mybot_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
